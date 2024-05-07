bf2bd358f9579a38bbdee15ebf36aa364ce37be6
Tene Tiitma <tenetiitma@gmail.com>
2024-05-07 23:18:57 +0300

    kkk adding

--- resources/views/livewire/k-k-k-page.blade.php
+++ resources/views/livewire/k-k-k-page.blade.php
@@ -1,3 +1,38 @@
-<div>
-    <h1>KKK</h1>
-</div>
+<section class="bg-gray-50">
+    <div class="max-w-screen-xl px-4 py-32 mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-xl mx-auto text-center">
+            <h1 class="text-3xl font-medium sm:text-5xl">
+                Korduma Kippuvad Küsimused
+            </h1>
+            <br><br>
+            <div class="sm:leading-relaxed">
+                <h1 class="text-xl font-semibold sm:text-2xl">Mis on tellimuste tarneaeg?</h1>
+                <p class="font-light">Tellimuste tarneaeg on kuni 2 nädalat. Juhul, kui tegu on erilahendustega või tarneaeg erineb eelpool nimetatust, siis antakse sellest eraldi e-maili või telefoni teel teada.</p>
+                <br><br>
+                <h1 class="text-xl font-semibold sm:text-2xl">Kas tooteid saab tellida telefoni teel?</h1>
+                <p class="font-light">Tooteid saab tellida ka telefoni teel. Võimalik kontakteeruda otse kudumite valmistajaga telefoni numbril
+                <p class="font-semibold">+3725101204</p>
+                </p>
+                <br><br>
+                <h1 class="text-xl font-semibold sm:text-2xl">Kas tooted on saadaval ka tavakaupluses?</h1>
+                <p class="font-light">Tooted on saadaval vaid e-poes.</p>
+                <br><br>
+                <h1 class="text-xl font-semibold sm:text-2xl">Kas saan tellitud toodet tagastada või vahetada teise mudeli vastu?</h1>
+                <p class="font-light">E-poest tellitud tooteid saab kas tagastada või vahetada suurust 14 päeva jooksul. Toote vahetamine teise mudeli vastu toimub erikokkuleppel, millega võivad
+                    kohanduda lisatasud. Täpsem info eraldi tagastustingimuste all.</p>
+                <br><br>
+                <h1 class="text-xl font-semibold sm:text-2xl">Kas kudumit on võimalik eelnevalt proovida?</h1>
+                <p class="font-light">Kudumid valmistatakse vastavalt tellimusele ja on võimalik proovida peale valmimist eraldi kokkuleppel ainult Kuressaares.</p>
+                <br><br>
+                <h1 class="text-xl font-semibold sm:text-2xl">Kas on võimalik tellida kudumit vastavalt enda erisoovidele?</h1>
+                <p class="font-light">Jah, kui see on teostatav. Selleks tuleb täita e-poe päringuvorm; saata e-mail või võtta ühendust telefoni teel numbril
+                <p class="font-semibold">+3725101204</p> (Võivad kohanduda lisatasud)</p>
+                <br><br>
+                <h1 class="text-xl font-semibold sm:text-2xl">Kas on võimalik osta kinkekaarti?</h1>
+                <p class="font-light">Kinkekaardid on e-poodi tulemas peatselt, seniks võta ühendust meiega <p class="font-semibold">+3725101204</p></p>
+            </div>
+
+
+        </div>
+    </div>
+</section>
