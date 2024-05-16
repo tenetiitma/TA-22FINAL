4a968481ec84d4c4362546d650f3654e6ad031e2
Tene Tiitma <tenetiitma@gmail.com>
2024-05-16 11:52:00 +0300

    policies updated

--- resources/views/livewire/privacy-policy-page.blade.php
+++ resources/views/livewire/privacy-policy-page.blade.php
@@ -1,3 +1,82 @@
-<div>
-    <h1>Privaatsustingimused</h1>
-</div>
+<section class="bg-gray-50">
+    <div class="max-w-screen-xl px-4 py-32 mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-screen-xl px-8 text-justify">
+            <h1 class="text-3xl mx-auto mb-8 text-center font-medium sm:text-5xl">
+                PRIVAATSUSTINGIMUSED
+            </h1>
+            <br><br>
+            <div class="sm:leading-relaxed">
+                <h1 class="text-xl font-semibold mb-4 sm:text-2xl">1. Üldsätted</h1>
+                <p class="font-light mb-2"><span class="font-semibold">1.1.</span> Käesolev privaatsuspoliitika reguleerib isikuandmete kogumist, töötlemist ja säilitamist käsitlevaid põhimõtteid. Isikuandmeid kogub töötleb ja säilitab isikuandmete vastutav töötleja <span class="font-semibold">Wööl Disain OÜ</span> (edaspidi andmetöötleja).</p>
+                <p class="font-light mb-2"><span class="font-semibold">1.2.</span> Andmesubjekt privaatsuspoliitika tähenduses on klient või muu füüsiline isik, kelle isikuandmeid andmetöötleja töötleb.</p>
+                <p class="font-light mb-2"><span class="font-semibold">1.3.</span> Klient privaatsuspoliitika tähenduses on igaüks, kes ostab andmetöötleja kodulehelt kaupu või teenuseid.</p>
+                <p class="font-light mb-2"><span class="font-semibold">1.4.</span> Andmetöötleja järgib õigusaktides sätestatud andmete töötlemise põhimõtteid, muuhulgas töötleb andmetöötleja isikuandmeid seaduslikult, õiglaselt ja turvaliselt. Andmetöötleja on võimeline kinnitama, et isikuandmeid on töödeldud vastavalt õigusaktides sätestatule.</p>
+                <br>
+                <h1 class="text-xl font-semibold mb-4 sm:text-2xl">2. Isikuandmete kogumine, töötlemine ja säilitamine</h1>
+                <p class="font-light mb-2"><span class="font-semibold">2.1.</span> Isikuandmed, mida andmetöötleja kogub, töötleb ja säilitab, on kogutud elektrooniliselt, peamiselt kodulehe ja e-posti vahendusel.</p>
+                <p class="font-light mb-2"><span class="font-semibold">2.2.</span> Oma isikuandmete jagamisega annab andmesubjekt andmetöötlejale õiguse koguda, korraldada, kasutada ja hallata privaatsuspoliitikas määratletud eesmärgil isikuandmeid, mida andmesubjekt otse või kaudselt kodulehel kaupu või teenuseid ostes andmetöötlejale jagab.</p>
+                <p class="font-light mb-2"><span class="font-semibold">2.3.</span> Andmesubjekt vastutab selle eest, et tema poolt esitatud andmed oleksid täpsed, õiged ja terviklikud. Teadlikult valeandmete esitamist peetakse privaatsuspoliitika rikkumiseks. Andmesubjekt on kohustatud andmetöötlejat viivitamatult teavitama esitatud andmete muutumisest.</p>
+                <p class="font-light mb-2"><span class="font-semibold">2.4.</span> Andmetöötleja ei vastuta andmesubjekti poolt valeandmete esitamisest põhjustatud kahju eest andmesubjektile või kolmandatele osapooltele.</p>
+                <br>
+                <h1 class="text-xl font-semibold mb-4 sm:text-2xl">3. Klientide isikuandmete töötlemine</h1>
+                <p class="font-light mb-2"><span class="font-semibold">3.1.</span> Andmetöötleja võib töödelda järgnevaid andmesubjekti isikuandmeid:
+                <ul class="ml-4 mt-2 mb-4 font-extralight italic">
+                    <li>3.1.1. Ees-ja perekonnanimi</li>
+                    <li>3.1.2. Sünnikuupäev</li>
+                    <li>3.1.3. Telefoninumber</li>
+                    <li>3.1.4. E-posti aadress</li>
+                    <li>3.1.5. Kohaletoimetamise aadress</li>
+                    <li>3.1.6. Arvelduskontonumber</li>
+                    <li>3.1.7. Maksekaardi detailid</li>
+                </ul>
+                </p>
+                <p class="font-light mb-2"><span class="font-semibold">3.2.</span> Lisaks eeltoodule on andmetöötlejal õigus koguda kliendi kohta andmeid, mis on kättesaadavad avalikes registrites.</p>
+                <p class="font-light mb-2"><span class="font-semibold">3.3.</span> Isikuandmete töötlemise õiguslik alus on isikuandmete kaitse üldmääruse paragrahv 6 lg 1 p-d a), b), c) ja f):
+                <ul class="ml-4 mt-2 mb-4 font-extralight italic">
+                    <li class="mb-2">a) andmesubjekt on andnud nõusoleku töödelda oma isikuandmeid ühel või mitmel konkreetsel eesmärgil</li>
+                    <li class="mb-2">b) isikuandmete töötlemine on vajalik andmesubjekti osalusel sõlmitud lepingu täitmiseks või lepingu sõlmimisele eelnevate meetmete võtmiseks vastavalt andmesubjekti taotlusele</li>
+                    <li class="mb-2">c) isikuandmete töötlemine on vajalik vastutava töötleja juriidilise kohustuse täitmiseks</li>
+                    <li class="mb-2">f) isikuandmete töötlemine on vajalik vastutava töötleja või kolmanda isiku õigustatud huvi korral, välja arvatud juhul, kui sellise huvi kaaluvad üles andmesubjekti huvid või põhiõigused ja -vabadused, mille nimel tuleb kaitsta isikuandmeid, eriti juhul kui andmesubjekt on laps</li>
+                </ul>
+                </p>
+                <p class="font-light mb-2"><span class="font-semibold">3.4.</span> Isikuandmete töötlemine vastavalt töötlemise eesmärgile:
+                <ul class="ml-4 mt-2 mb-4 font-extralight italic">
+                    <li class="mb-2">3.4.1. Töötlemise eesmärk – julgeolek ja turvalisus
+                        <p>Isikuandmete säilitamise maksimaalne aeg – vastavalt seaduses nimetatud tähtaegadele</p>
+                    </li>
+                    <li class="mb-2">3.4.2. Töötlemise eesmärk – tellimuse töötlemine
+                        <p>Isikuandmete säilitamise maksimaalne aeg – 3 aastat</p>
+                    </li>
+                    <li class="mb-2">3.4.3. Töötlemise eesmärk – e-poe teenuste toimimise tagamine
+                        <p>Isikuandmete säilitamise maksimaalne aeg – 5 aastat</p>
+                    </li>
+                    <li class="mb-2">3.4.4. Töötlemise eesmärk – kliendihaldus
+                        <p>Isikuandmete säilitamise maksimaalne aeg – 7 aastat või kuni kliendi kustutamistaotluse esitamiseni. Isikuandmete kustutamiseks tuleb võtta ühendust klienditoega e-posti teel. Kustutamistaotlusele vastatakse mitte hiljem kui kuu aja jooksul ning täpsustakse andmete kustutamise perioodi</p>
+                    </li>
+                    <li class="mb-2">3.4.5. Töötlemise eesmärk – finantstegevus, raamatupidamine
+                        <p>Isikuandmete säilitamise maksimaalne aeg – vastavalt seaduses nimetatud tähtaegadele</p>
+                    </li>
+                    <li class="mb-2">3.4.6. Töötlemise eesmärk – turundus
+                        <p>Isikuandmete säilitamise maksimaalne aeg – 7 aastat või kuni kliendi kustutamistaotluse esitamiseni. Isikuandmete kustutamiseks tuleb võtta ühendust klienditoega e-posti teel. Kustutamistaotlusele vastatakse mitte hiljem kui kuu aja jooksul ning täpsustakse andmete kustutamise perioodi</p>
+                    </li>
+                </ul>
+                </p>
+                <p class="font-light mb-2"><span class="font-semibold">3.5.</span> Andmetöötlejal on õigus klientide isikuandmeid jagada kolmandate isikutega, kelleks on näiteks volitatud andmetöötlejad, raamatupidajad, transpordi- ja kullerettevõtted, ülekandeteenuseid pakkuvad ettevõtted Andmetöötleja on isikuandmete vastutav töötleja. Andmetöötleja edastab maksete teostamiseks vajalikud isikuandmed volitatud töötleja Maksekeskus AS-ile.</p>
+                <p class="font-light mb-2"><span class="font-semibold">3.6.</span> Andmesubjekti isikuandmete töötlemisel ja säilitamisel rakendab andmetöötleja organisatoorseid ja tehnilisi meetmeid, mis tagavad isikuandmete kaitse juhusliku või ebaseadusliku hävitamise, muutmise, avalikustamise ja mis tahes muu ebaseadusliku töötlemise eest.</p>
+                <p class="font-light mb-2"><span class="font-semibold">3.7.</span> Andmetöötleja säilitab andmesubjektide andmeid sõltuvalt töötlemise eesmärgist, kuid mitte kauem kui 3 aastat aastat.</p>
+                <br>
+                <h1 class="text-xl font-semibold mb-4 sm:text-2xl">4. Andmesubjekti õigused</h1>
+                <p class="font-light mb-2"><span class="font-semibold">4.1.</span> Andmesubjektil on õigus saada ligipääs oma isikuandmetele ning nendega tutvuda.</p>
+                <p class="font-light mb-2"><span class="font-semibold">4.2.</span> Andmesubjektil on õigus saada informatsiooni tema isikuandmete töötlemise kohta.</p>
+                <p class="font-light mb-2"><span class="font-semibold">4.3.</span> Andmesubjektil on õigus täiendada või parandada ebatäpseid andmeid.</p>
+                <p class="font-light mb-2"><span class="font-semibold">4.4.</span> Kui andmetöötleja töötleb andmesubjekti isikuandmeid andmesubjekti nõusoleku alusel, siis on andmesubjektil õigus igal ajal nõusolek tagasi võtta.</p>
+                <p class="font-light mb-2"><span class="font-semibold">4.5.</span> Andmesubjekt saab õiguste teostamiseks pöörduda e-poe klienditoe poole aadressil <span class="font-semibold">woolkudumid@gmail.com</span>.</p>
+                <p class="font-light mb-2"><span class="font-semibold">4.6.</span> Andmesubjektil on oma õiguste kaitseks võimalik esitada kaebus Andmekaitse Inspektsioonile.</p>
+                <br>
+                                <h1 class="text-xl font-semibold mb-4 sm:text-2xl">5. Lõppsätted</h1>
+                <p class="font-light mb-2"><span class="font-semibold">5.1.</span> Käesolevad andmekaitsetingimused on koostatud kooskõlas Euroopa Parlamendi ja nõukogu määrusega (EL) nr 2016/679 füüsiliste isikute kaitse kohta isikuandmete töötlemisel ja selliste andmete vaba liikumise ning direktiivi 95/46 / EÜ kehtetuks tunnistamise kohta / EÜ (isikuandmete kaitse üldmäärus), Eesti Vabariigi isikuandmete kaitse seadusega ning Eesti Vabariigi ja Euroopa Liidu õigusaktidega.</p>
+                <p class="font-light mb-2"><span class="font-semibold">5.2.</span> Andmetöötlejal on õigus andmekaitsetingimusi osaliselt või täielikult muuta, teavitades andmesubjekte muudatustest kodulehe <span class="font-semibold">www.wöölkudumid.ee</span> kaudu.</p>
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/livewire/return-policy-page.blade.php
+++ resources/views/livewire/return-policy-page.blade.php
@@ -1,3 +1,35 @@
-<div>
-    <h1>Tagastamistingimused</h1>
-</div>
+<section class="bg-gray-50">
+    <div class="max-w-screen-xl px-4 py-32 mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-screen-xl px-8 text-justify">
+            <h1 class="text-3xl mx-auto mb-8 text-center font-medium sm:text-5xl">
+                TAGASTAMISTINGIMUSED
+            </h1>
+            <br><br>
+            <div class="sm:leading-relaxed">
+                <ul class="list-decimal font-light ml-4">
+                    <li>Pärast tellimuse kättesaamist kehtib Ostjale 14-päevane tagastamisõigus.</li>
+                    <br>
+                    <li>Toote tagastamise soovi korral tuleb võtta ühendust meiliaadressil <span class="font-semibold">woolkudumid@gmail.com</span> ning täita tagastusavaldus.</li>
+                    <br>
+                    <li>Pärast tellimuse kättesaamist kehtib Ostjale 14-päevane tagastamisõigus.</li>
+                    <br>
+                    <li>Toote võib tagastada juhul, kui:
+                        <ul class="list-disc ml-4 mt-2">
+                            <li>Toode on komplektne koos kõigi lisadega, mis kaasas oli</li>
+                            <li>Toode on kasutamata</li>
+                            <li>Toote pakend on rikkumata</li>
+                            <li>Toode tagastatakse koos tagastusavaldusega</li>
+                        </ul>
+                    </li>
+                    <br>
+                    <li>Toote tagastuskulud kannab Ostja.</li>
+                    <br>
+                    <li>Toote tagastamise korral kantakse toote eest tasutud summa Ostja pangakontole 10 tööpäeva jooksul.</li>
+                    <br>
+                    <li>Juhul kui tootel esineb defekte või toode ei vasta soovitule, tuleb kohe võtta ühendust Müüjaga. Müüja saadab korras toote Ostjale 14 päeva jooksul pärast defektse/sobivusele mitte vastava toote kättesaamise päevast. Müüja tasub sellisel juhul saatmiskulud ise.</li>
+                </ul>
+                <br>
+            </div>
+        </div>
+    </div>
+</section>
