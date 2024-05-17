92f64f65c8d084f2ae845e1a6be1aa993c93d286
Tene Tiitma <tenetiitma@gmail.com>
2024-05-17 09:26:40 +0300

    Fixed about us visual on mobile

--- resources/views/livewire/about-page.blade.php
+++ resources/views/livewire/about-page.blade.php
@@ -2,26 +2,25 @@
     <div class="max-w-screen-xl px-4 py-32 mx-auto sm:px-6 lg:px-8">
         <div class="max-w-screen-xl px-8 text-justify">
             <h1 class="text-4xl mx-auto mb-8 text-center allura-regular text-stone-600 font-medium sm:text-5xl">
-                Mis on Wööl?
+                Mida tähendab Wööl?
             </h1>
             <br><br>
             <div class="sm:leading-relaxed">
                 <div class="grid grid-cols-1 font-light lg:grid-cols-2 gap-8 items-center">
-                    <div class="lg:order-2">
+                    <div class="order-1 lg:order-4">
                         <p class="text-lg mb-6 leading-relaxed text-center lg:text-justify">Valikut on kogu perele ja kui on tunne, et ei leia seda sobivat toodet, siis on
                             võimalik panna kokku kudum vastavalt enda erisoovidele ja nägemusele.</p>
                         <p class="text-lg mb-6 leading-relaxed text-center lg:text-justify">Toodete valikust leiab erinevaid kudumeid ja samuti aksessuaare
                             nagu mütsid, sallid ning peapaelad.</p>
                         <p class="text-lg mb-2 leading-relaxed text-center font-bold lg:text-justify">Kudumid on inspireeritud erinevatest loodusvärvidest ning kaunitest Eesti paikadest. </p>
                     </div>
-                    <div class="relative lg:order-1">
+                    <div class="relative order-2 lg:order-3">
                         <div class="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-transparent to-white mix-blend-multiply"></div>
                         <div class="fade-container lg:right-fade">
                             <img class="w-full h-auto" src="{{ asset('images/abt2.jpeg') }}" alt="About Image 2">
                         </div>
-
                     </div>
-                    <div>
+                    <div class="order-4 lg:order-1">
                         <p class="text-lg mb-6 leading-relaxed text-center lg:text-justify">
                             <span>WÖÖL</span> on saanud oma tähenduse selle järgi, et kasutame enda toodetes kvaliteetset
                             villa ning rahvuslikku vöökirja.
@@ -32,12 +31,11 @@
                         </p>
                         <p class="text-lg mb-2 leading-relaxed text-center font-bold lg:text-justify">Kudumites kasutatakse vaid naturaalset toormaterjali ning peetakse lugu jätkusuutlikusest. Wööli tooted on valmistatud lamba- ja meriinovillast, mis on 100% naturaalsed.</p>
                     </div>
-                    <div class="relative">
+                    <div class="relative order-3 lg:order-2">
                         <div class="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-transparent to-white mix-blend-multiply"></div>
                         <div class="fade-container lg:left-fade">
                             <img class="w-full h-auto" src="{{ asset('images/abt1.jpeg') }}" alt="About Image 1">
                         </div>
-
                     </div>
                 </div>
             </div>
