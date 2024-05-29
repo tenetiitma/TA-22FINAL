cff8e0509cd703ddffdcd057bba95f3be6e9cb79
Tene Tiitma <tenetiitma@gmail.com>
2024-05-29 13:27:12 +0300

    visual fixes

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,15 +1,17 @@
 <footer class="bg-gray-50">
     <div class="max-w-screen-xl px-4 py-8 mx-auto text-center sm:px-6 lg:px-8">
-        <div class="grid grid-cols-1 gap-8 lg:grid-cols-3 text-center lg:text-left">
+        <div class="grid grid-cols-1 gap-12 lg:grid-cols-3 text-center lg:text-left">
             <div class="flex justify-center">
-                <ul class="max-w-sm mt-4 text-gray-500 font-light text-sm">
-                    <li><img src="{{ asset('images/logotrans.png')}}" class="h-28 w-auto mx-auto lg:mx-0" alt="Logo"></li>
+                <ul class="max-w-sm mt-4 text-stone-500 font-light text-sm">
+                    <li><img src="{{ asset('images/tranlog.png')}}" class="h-10 mb-4 w-auto mx-auto lg:mx-0" alt="Logo"></li>
+                    <li><p>(+372) 51 01 204</p></li>
+                    <li><a href="mailto:woolkudumid@gmail.com" class="hover:underline">woolkudumid@gmail.com</a></li>
                 </ul>
             </div>
             
-            <div>
-                <p class="text-gray-600 font-semibold">E-pood</p>
-                <ul class="mt-4 text-gray-500 font-light text-sm">
+            <div class="md:border-l md:border-gray-200 md:pl-12">
+                <p class="text-stone-600 font-semibold">E-pood</p>
+                <ul class="mt-4 text-stone-500 font-light text-sm">
                     <li class="mb-2"><a href="/kkk" class="hover:underline">KKK</a></li>
                     <li class="mb-2"><a href="/sales-terms" class="hover:underline">Müügitingimused</a></li>
                     <li class="mb-2"><a href="/return-policy" class="hover:underline">Tagastamistingimused</a></li>
@@ -18,18 +20,18 @@
             </div>
             
             <div>
-                <p class="text-gray-600 font-semibold">Wööl'i perekond</p>
-                <ul class="mt-4 text-gray-500 font-light text-sm">
+                <p class="text-stone-600 font-semibold">Wööl'i perekond</p>
+                <ul class="mt-4 text-stone-500 font-light text-sm">
                     <li class="mb-2"><a href="/about" class="hover:underline">Meist</a></li>
                     <li class="mb-2"><a href="/contact" class="hover:underline">Kontakt</a></li>
                 </ul>
                 <div class="flex justify-center lg:justify-start gap-4 mt-4">
-                    <a href="https://www.facebook.com/Woolkudumid" target="_blank" class="text-gray-500 hover:text-gray-700"><x-brand.facebook /></a>
-                    <a href="https://www.instagram.com/woolkudumid/" target="_blank" class="text-gray-500 hover:text-gray-700"><x-brand.instagram /></a>
+                    <a href="https://www.facebook.com/Woolkudumid" target="_blank" class="text-stone-500 hover:text-stone-700"><x-brand.facebook /></a>
+                    <a href="https://www.instagram.com/woolkudumid/" target="_blank" class="text-stone-500 hover:text-stone-700"><x-brand.instagram /></a>
                 </div>
             </div>
         </div>
-        <p class="pt-4 mt-8 text-sm text-gray-500 border-t border-gray-200">
+        <p class="pt-4 mt-8 text-sm text-stone-400 border-t border-gray-200">
             &copy; {{ now()->year }} WÖÖL
         </p>
     </div>
